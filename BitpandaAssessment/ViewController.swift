bd623b066b5f9d2c84cc1c99791831b690347aec
Osama Fawzi <o.fawzi@nousdigital.net>
2022-04-23 05:53:53 +0200

    Initial Commit

---
+++ BitpandaAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitpandaAssessment
+//
+//  Created by Osama Fawzi on 23.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
